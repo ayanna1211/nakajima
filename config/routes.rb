ffa68abf9107a8fb2be68b2ec70d5d58e8f199fd
gkvitthao <gkvit.thao@gmail.com>
2015-09-09 09:33:21 +0700

    create db users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   
+  resources :microposts
+  resources :users
   get '/index', to: 'top#index'
+  get '/users', to: 'users#index'  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
